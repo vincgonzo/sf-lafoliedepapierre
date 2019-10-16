4f2995d5ad2ef6f94b25faa8afacfbd8846376dd
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-10-16 11:41:41 +0200

    refactoring twig tpl

--- src/LFDPP/AdminBundle/Controller/DefaultController.php
+++ src/LFDPP/AdminBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('@Admin/Default/index.html.twig');
+        return $this->render('@Admin/Front/index.html.twig');
     }
 }
