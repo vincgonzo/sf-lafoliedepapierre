39261b97b30975161722310210d54b55e397569c
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-10-16 11:52:11 +0200

    routing by annotation

--- src/LFDPP/AdminBundle/Controller/FrontController.php
+++ src/LFDPP/AdminBundle/Controller/FrontController.php
@@ -4,9 +4,13 @@ namespace LFDPP\AdminBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends Controller
 {
+  /**
+   * @Route("/hello-world", name="hello-world")
+   */
   public function index()
   {
     return  $this->render('@Admin/Front/content.html.twig');
