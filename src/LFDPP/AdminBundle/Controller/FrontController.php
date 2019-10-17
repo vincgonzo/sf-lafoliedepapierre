0086b6d345be3283146d9ec5fcb033200340629e
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-10-17 14:30:47 +0200

    test redirection & flashBags

--- src/LFDPP/AdminBundle/Controller/FrontController.php
+++ src/LFDPP/AdminBundle/Controller/FrontController.php
@@ -4,15 +4,28 @@ namespace LFDPP\AdminBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+   * @Route("/front")
+   */
 class FrontController extends Controller
 {
   /**
-   * @Route("/hello-world", name="hello-world")
+   * @Route("/hello-world", name="hello_world")
    */
   public function index()
   {
     return  $this->render('@Admin/Front/content.html.twig');
+  }
+
+  /**
+   * @Route("/page/{id}", name="page_request", requirements={"page"= "\d+"}, defaults={"page" = 1})
+   */
+  public function pageAction($id, Request $request)
+  {
+      $this->addFlash('notice', 'page not found !!!');
+      return $this->redirectToRoute("hello_world");
   }
 }
