8b5cc334159ee831e3dd9255eff098ef446124a9
vincgonzo@hotmail.fr <vincgonzo@hotmail.fr>
2019-10-21 13:45:04 +0200

    price type update / delete flow - end CRUD for this

--- src/LFDPP/AdminBundle/Controller/Admin/Forms/PriceTypeController.php
+++ src/LFDPP/AdminBundle/Controller/Admin/Forms/PriceTypeController.php
@@ -47,7 +47,7 @@ class PriceTypeController extends Controller
         }
         }
 
-        return $this->render('AdminBundle:Admin\Forms\PriceType:add.html.twig', array(
+        return $this->render('AdminBundle:Admin\Forms\PriceType:form.html.twig', array(
             "form" => $form->createView() 
         ));
     }
@@ -64,6 +64,54 @@ class PriceTypeController extends Controller
         ));
     } 
 
+    
+    /**
+     * @Route("/edit/{id}", name="admin_form_price_type_edit", requirements={"id" = "\d+"})
+     */
+    public function editAction($id, Request $request)
+    {
+        $priceType = $this->getDoctrine()->getRepository("AdminBundle:PriceType")->find($id);
+
+        $form = $this->get('form.factory')->createBuilder(FormType::class, $priceType)
+        ->add('type',      TextType::class)
+        ->add('days',     IntegerType::class)
+        ->add('save',      SubmitType::class)
+        ->getForm()
+        ;
+
+        if ($request->isMethod('POST')) {
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($priceType);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Price Type bien mis à jour.');
+
+            return $this->redirectToRoute('admin_form_price_type_ind', array('id' => $priceType->getId()));
+        }
+        }
+
+        return $this->render('AdminBundle:Admin\Forms\PriceType:form.html.twig', array(
+            "form" => $form->createView() 
+        ));
+    } 
+
+    /**
+     * @Route("/delete/{id}", name="admin_form_price_type_delete", requirements={"id" = "\d+"})
+     */
+    public function deleteAction($id, Request $request)
+    {
+        $priceType = $this->getDoctrine()->getRepository("AdminBundle:PriceType")->find($id);
+        $_em = $this->getDoctrine()->getManager();
+        $_em->remove($priceType);
+        $_em->flush();
+
+        $request->getSession()->getFlashBag()->add('notice', 'Price Type deleted.');
+
+        return $this->redirectToRoute('admin_form_price_type');
+    } 
     
     /**
      * @Route("/", name="admin_form_price_type")
